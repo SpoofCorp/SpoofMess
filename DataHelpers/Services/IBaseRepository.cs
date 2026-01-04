d49829ac4c3dc11b64f9b594fa465337c05ff2f9
Pychka <kronossluga@gmail.com>
2026-01-04 22:03:46 +0500

    feat: add interface for base repositories

---
+++ DataHelpers/Services/IBaseRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace DataHelpers.Services;
+
+public interface IBaseRepository<T, TKey> where T : IdentifiedEntity<TKey>
+{
+    public ValueTask<T?> GetByIdAsync(TKey id);
+    public Task AddAsync(T entity);
+    public Task DeleteAsync(T entity);
+    public Task SoftDeleteAsync(T entity);
+    public Task UpdateAsync(T entity);
+    public Task UpdateRangeAsync(List<T> entities);
+}
