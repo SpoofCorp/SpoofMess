58d06e7b83cb166180d851b02d990845a65960d2
Pychka <kronossluga@gmail.com>
2026-01-05 15:38:51 +0500

    refactor: change base virtual methods to abstract and forces implement in inheritors

--- DataHelpers/Services/BaseRepository.cs
+++ DataHelpers/Services/BaseRepository.cs
@@ -4,28 +4,11 @@ namespace DataHelpers.Services;
 
 public abstract class BaseRepository
 {
-    protected async virtual ValueTask<TEntity?> GetAsync<TEntity>(
-        string key,
-        Func<Task<TEntity>> function,
-        TimeSpan? expiration = null) where TEntity : IIdentifiedEntity?
-    { throw new NotImplementedException(); }
+    protected abstract ValueTask<TEntity?> GetAsync<TEntity>(string key, Func<Task<TEntity>> function, TimeSpan? expiration = null) where TEntity : IIdentifiedEntity?;
 
-    protected async virtual ValueTask<TEntity> ChangeState<TEntity>(
-        DbContext context,
-        TEntity obj,
-        EntityState state = EntityState.Added,
-        TimeSpan? expiration = null) where TEntity : IIdentifiedEntity
-    { throw new NotImplementedException(); }
+    protected abstract ValueTask<TEntity> ChangeState<TEntity>(DbContext context, TEntity obj, EntityState state = EntityState.Added, TimeSpan? expiration = null) where TEntity : IIdentifiedEntity;
 
-    protected virtual ValueTask<List<TEntity>> ChangeStates<TEntity>(
-        DbContext context,
-        (TEntity obj, EntityState state)[] entities,
-        TimeSpan? expiration = null) where TEntity : IIdentifiedEntity
-    { throw new NotImplementedException(); }
+    protected abstract ValueTask<List<TEntity>> ChangeStates<TEntity>(DbContext context, (TEntity obj, EntityState state)[] entities, TimeSpan? expiration = null) where TEntity : IIdentifiedEntity;
 
-    protected virtual ValueTask<List<T>?> GetMany<T>(
-        string key,
-        Func<Task<List<T>>> function,
-        TimeSpan? expiration = null) where T : IIdentifiedEntity
-    { throw new NotImplementedException(); }
+    protected abstract ValueTask<List<T>?> GetMany<T>(string key, Func<Task<List<T>>> function, TimeSpan? expiration = null) where T : IIdentifiedEntity;
 }
