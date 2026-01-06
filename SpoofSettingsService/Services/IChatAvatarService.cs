f50301484aca38dc9c411888ac8906254ab1be62
Pychka <kronossluga@gmail.com>
2026-01-06 21:44:11 +0500

    feat: add ChatAvatarService interface

---
+++ SpoofSettingsService/Services/IChatAvatarService.cs
@@ -0,0 +1,16 @@
+﻿using CommonObjects.Requests;
+using CommonObjects.Responses;
+using CommonObjects.Results;
+
+namespace SpoofSettingsService.Services;
+
+public interface IChatAvatarService
+{
+    public Task<Result> SetAvatar(SetChatAvatarRequest request);
+
+    public Task<Result<ChatAvatarResponse>> GetAvatar(GetChatAvatarRequest request);
+
+    public Task<Result<List<ChatAvatarResponse>>> GetAvatars(GetChatAvatarRequest request);
+
+    public Task<Result> RemoveAvatar(RemoveChatAvatarRequest request);
+}
