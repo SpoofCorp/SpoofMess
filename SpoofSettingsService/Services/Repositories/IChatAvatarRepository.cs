00374ab36dadf8f8d4a1bab78bd5d7c873184e89
Pychka <kronossluga@gmail.com>
2026-01-06 21:57:06 +0500

    feat: add interface repository for ChatAvatar

---
+++ SpoofSettingsService/Services/Repositories/IChatAvatarRepository.cs
@@ -0,0 +1,13 @@
+﻿using DataHelpers.Services;
+using SpoofSettingsService.Models;
+
+namespace SpoofSettingsService.Services.Repositories;
+
+public interface IChatAvatarRepository : IBaseRepository<ChatAvatar, Guid>
+{
+    public Task<ChatAvatar?> GetActualChatAvatarById(Guid chatId);
+
+    public Task<List<ChatAvatar>?> GetChatAvatarsById(Guid chatId);
+
+    public Task<bool> TryDeleteAvatarByIds(Guid chatId, Guid fileId);
+}
