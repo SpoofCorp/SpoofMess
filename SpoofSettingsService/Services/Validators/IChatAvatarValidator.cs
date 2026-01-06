9fc5d95e4a417d5bc67e5f6d24e8b5c0f80f1740
Pychka <kronossluga@gmail.com>
2026-01-06 21:43:41 +0500

    feat: add IChatAvatarValidator

---
+++ SpoofSettingsService/Services/Validators/IChatAvatarValidator.cs
@@ -0,0 +1,13 @@
+﻿using CommonObjects.Results;
+using SpoofSettingsService.Models;
+
+namespace SpoofSettingsService.Services.Validators;
+
+public interface IChatAvatarValidator
+{
+    public Result IsNullOrDeleted(ChatAvatar? chatAvatar);
+
+    public Result FileIsNullOrDeleted(ChatAvatar? chatAvatar);
+
+    public Result AvatarsIsNullOrEmpty(List<ChatAvatar>? chatAvatars);
+}
