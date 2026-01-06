f4f673d77a3069665cae6f442b606789c343973b
Pychka <kronossluga@gmail.com>
2026-01-06 21:42:57 +0500

    feat: add FileMetadatumSetter

---
+++ SpoofSettingsService/Setters/FileMatadatumSetter.cs
@@ -0,0 +1,10 @@
+﻿using CommonObjects.DTO;
+using SpoofSettingsService.Models;
+
+namespace SpoofSettingsService.Setters;
+
+public static class FileMatadatumSetter
+{
+    public static FileMetadata Set(this FileMetadatum fileMetadatum) =>
+        new() { Name = "", Size = 42 };
+}
