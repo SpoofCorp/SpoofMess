3b8f3e23bc37fd4710507abb04a44390f9435815
Pychka <kronossluga@gmail.com>
2026-01-06 21:55:52 +0500

    feat: add IChatAvatarService realization. Realized: add, get(actual/all),set, remove chat avatar

---
+++ SpoofSettingsService/ServiceRealizations/ChatAvatarService.cs
@@ -0,0 +1,105 @@
+﻿using AdditionalHelpers.Services;
+using CommonObjects.Requests;
+using CommonObjects.Responses;
+using CommonObjects.Results;
+using SpoofSettingsService.Models;
+using SpoofSettingsService.Services;
+using SpoofSettingsService.Services.Publisher;
+using SpoofSettingsService.Services.Repositories;
+using SpoofSettingsService.Services.Validators;
+using SpoofSettingsService.Setters;
+
+namespace SpoofSettingsService.ServiceRealizations;
+
+public class ChatAvatarService(ILoggerService loggerService, IChatAvatarPublisherService chatAvatarPublisher, IChatAvatarRepository chatAvatarRepository, IChatAvatarValidator chatAvatarValidator) : IChatAvatarService
+{
+    private readonly ILoggerService _loggerService = loggerService;
+    private readonly IChatAvatarRepository _chatAvatarRepository = chatAvatarRepository;
+    private readonly IChatAvatarValidator _chatAvatarValidator = chatAvatarValidator;
+    private readonly IChatAvatarPublisherService _chatAvatarPublisher = chatAvatarPublisher;
+
+    public async Task<Result<ChatAvatarResponse>> GetAvatar(GetChatAvatarRequest request)
+    {
+        try
+        {
+            ChatAvatar? avatar = await _chatAvatarRepository.GetActualChatAvatarById(request.ChatId);
+            Result result = _chatAvatarValidator.IsNullOrDeleted(avatar);
+            if (!result.Success)
+                return Result<ChatAvatarResponse>.From(result);
+
+            return Result<ChatAvatarResponse>.SuccessResult(new() { FileId = avatar!.FileId!.Value, FileMetadata = avatar.File!.Set() });
+        }
+        catch (Exception ex)
+        {
+            _loggerService.Error("DataBase error", ex);
+            return Result<ChatAvatarResponse>.ErrorResult("DataBase error");
+        }
+    }
+
+    public async Task<Result<List<ChatAvatarResponse>>> GetAvatars(GetChatAvatarRequest request)
+    {
+        try
+        {
+
+            List<ChatAvatar>? avatars = await _chatAvatarRepository.GetChatAvatarsById(request.ChatId);
+            Result result = _chatAvatarValidator.AvatarsIsNullOrEmpty(avatars);
+            if (!result.Success)
+                return Result<List<ChatAvatarResponse>>.From(result);
+
+            ChatAvatar avatar = null!;
+            Result<List<ChatAvatarResponse>> response = Result<List<ChatAvatarResponse>>.OkResult([]);
+
+            for (int i = 0; i < avatars!.Count; i++)
+            {
+                avatar = avatars[i];
+                response.Body!.Add(new() { FileId = avatars[i].FileId!.Value, FileMetadata = avatars[i].File!.Set() });
+            }
+
+            return response;
+        }
+        catch (Exception ex)
+        {
+            _loggerService.Error("DataBase error", ex);
+            return Result<List<ChatAvatarResponse>>.ErrorResult("DataBase error");
+        }
+    }
+
+    public async Task<Result> RemoveAvatar(RemoveChatAvatarRequest request)
+    {
+        try
+        {
+            bool result = await _chatAvatarRepository.TryDeleteAvatarByIds(request.ChatId, request.FileId);
+
+            return result ? Result.OkResult() : Result.BadRequest("Invalid id");
+        }
+        catch (Exception ex)
+        {
+            _loggerService.Error("DataBase error", ex);
+            return Result.ErrorResult("DataBase error");
+        }
+    }
+
+    public async Task<Result> SetAvatar(SetChatAvatarRequest request)
+    {
+        try
+        {
+            ChatAvatar chatAvatar = new()
+            {
+                ChatId = request.ChatId,
+                File = request.Metadata.Set()
+            };
+            chatAvatar.File.Id = request.FileId;
+
+            await _chatAvatarRepository.AddAsync(chatAvatar);
+
+            _ = Task.Run(async () => await _chatAvatarPublisher.Publish(chatAvatar));
+
+            return Result.OkResult();
+        }
+        catch (Exception ex)
+        {
+            _loggerService.Error("DataBase error", ex);
+            return Result.ErrorResult("DataBase error");
+        }
+    }
+}
