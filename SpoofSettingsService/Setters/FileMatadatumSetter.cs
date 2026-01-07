d18418b3a32158b1c4edce7ad0b768b5e217004a
Pychka <kronossluga@gmail.com>
2026-01-07 18:21:30 +0500

    feat: realize FileMetadatumSetter FileMetadatum <-> FileMetadata

--- SpoofSettingsService/Setters/FileMatadatumSetter.cs
+++ SpoofSettingsService/Setters/FileMatadatumSetter.cs
@@ -6,5 +6,8 @@ namespace SpoofSettingsService.Setters;
 public static class FileMatadatumSetter
 {
     public static FileMetadata Set(this FileMetadatum fileMetadatum) =>
-        new() { Name = "", Size = 42 };
+        new() { Name = fileMetadatum.Name, Bucket = fileMetadatum.Bucket, ObjectKey = fileMetadatum.ObjectKey, Size = fileMetadatum.Size }; 
+    
+    public static FileMetadatum Set(this FileMetadata fileMetadata) =>
+        new() { Size = fileMetadata.Size, Name = fileMetadata.Name, Bucket = fileMetadata.Bucket, ObjectKey = fileMetadata.ObjectKey };
 }
