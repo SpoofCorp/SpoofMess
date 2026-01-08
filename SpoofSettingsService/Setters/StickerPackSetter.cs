d692f2dd9b649681cd4bf9e8bf60ddc8973caea7
Pychka <kronossluga@gmail.com>
2026-01-08 14:33:10 +0500

    feat: add new setters: Sticker and StickerPack

---
+++ SpoofSettingsService/Setters/StickerPackSetter.cs
@@ -0,0 +1,14 @@
+﻿using CommonObjects.Responses;
+using SpoofSettingsService.Models;
+
+namespace SpoofSettingsService.Setters;
+
+public static class StickerPackSetter
+{
+    public static GetStickerPackResponse Response(this StickerPack stickerPack) =>
+        new()
+        {
+            FileId = stickerPack.PreviewId ?? Guid.Empty,
+            Stickers = [.. stickerPack.Stickers.Select(x => x.Set())]
+        };
+}
---
+++ SpoofSettingsService/Setters/StickerSetter.cs
@@ -0,0 +1,15 @@
+﻿using CommonObjects.DTO;
+using SpoofSettingsService.Models;
+
+namespace SpoofSettingsService.Setters;
+
+public static class StickerSetter
+{
+    public static StickerDTO Set(this Sticker sticker) =>
+        new()
+        {
+            FileId = sticker.FileId ?? Guid.Empty,
+            Id = sticker.Id,
+            Name = sticker.Title,
+        };
+}
