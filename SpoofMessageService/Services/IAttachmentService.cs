698b09b2c35dfaf70e3712bce2c50fd539705d41
Pychka <kronossluga@gmail.com>
2026-01-13 23:56:08 +0500

    feat: add interfaces for db models services

---
+++ SpoofMessageService/Services/IAttachmentService.cs
@@ -0,0 +1,10 @@
+﻿using CommonObjects.Requests;
+using CommonObjects.Results;
+
+namespace SpoofMessageService.Services;
+
+public interface IAttachmentService
+{
+    public Task<Result> AddAttachment(AddAttachmentRequest request);
+    public Task<Result> RemoveAttachment(RemoveAttachmentRequest request);
+}
---
+++ SpoofMessageService/Services/IMessageService.cs
@@ -0,0 +1,13 @@
+﻿using CommonObjects.Requests;
+using CommonObjects.Results;
+
+namespace SpoofMessageService.Services;
+
+public interface IMessageService
+{
+    public Task<Result> SendMessage(CreateMessageRequest request);
+
+    public Task<Result> DeleteMessage(DeleteMessageRequest request);
+
+    public Task<Result> EditMessage(EditMessageRequest request);
+}
