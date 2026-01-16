e740fc92ad91c6bd7d0cce61a96121e4e352832e
Pychka <kronossluga@gmail.com>
2026-01-16 22:53:59 +0500

    feat: add db models & his validators

---
+++ SpoofFileService/ServiceRealizations/Validators/FileValidator.cs
@@ -0,0 +1,21 @@
+﻿using CommonObjects.Results;
+using DataSaveHelpers.ServiceRealizations;
+using SpoofFileService.Models;
+using SpoofFileService.ServiceRealizatoionss.Validators;
+
+namespace SpoofFileService.ServiceRealizations.Validators;
+
+public class FileValidator : SoftDeletableValidator<FileObject>, IFileValidator
+{
+    public Result IsAvailableAndFileExists(FileObject? obj)
+    {
+        Result result = IsAvailable(obj);
+        if (!result.Success)
+            return result;
+
+        if (File.Exists(obj!.FilePath))
+            return Result.OkResult();
+
+        return Result.BadRequest("File is not exist");
+    }
+}
---
+++ SpoofFileService/Services/Validators/IFileValidator.cs
@@ -0,0 +1,10 @@
+﻿using CommonObjects.Results;
+using DataSaveHelpers.Services;
+using SpoofFileService.Models;
+
+namespace SpoofFileService.ServiceRealizatoionss.Validators;
+
+public interface IFileValidator : ISoftDeletableValidator<FileObject>
+{
+    public Result IsAvailableAndFileExists(FileObject? fileObject)
+}
