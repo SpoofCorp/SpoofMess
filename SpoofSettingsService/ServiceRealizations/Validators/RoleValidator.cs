e492c742b0c4e3a7ccaaf46cf6ffe8bcee91ba77
Pychka <kronossluga@gmail.com>
2026-01-22 12:14:41 +0500

    feat: add roleValidator & remove baseValidator

---
+++ SpoofSettingsService/ServiceRealizations/Validators/RoleValidator.cs
@@ -0,0 +1,10 @@
+﻿using DataSaveHelpers.ServiceRealizations;
+using SpoofSettingsService.Models;
+using SpoofSettingsService.Services.Validators;
+
+namespace SpoofSettingsService.ServiceRealizations.Validators;
+
+public class RoleValidator : SoftDeletableValidator<Role>, IRoleValidator
+{
+
+}
--- SpoofSettingsService/Services/Validators/IBaseValidator.cs
+++
@@ -1,9 +0,0 @@
-﻿using CommonObjects.Results;
-using DataSaveHelpers;
-
-namespace SpoofSettingsService.Services.Validators;
-
-public interface IBaseValidator
-{
-    public Result ValidateExist<T>(T? entity) where T : ISoftDeletable;
-}
