76d2fcc5fd1b6090de2b9b6e1640aff397e0f6f7
Pychka <kronossluga@gmail.com>
2026-02-16 22:47:22 +0500

    feat: add RuleParserService

---
+++ SpoofMessageService/Services/IRuleParserService.cs
@@ -0,0 +1,10 @@
+﻿using CommunicationLibrary.Communication;
+using SpoofMessageService.Models.Enums;
+
+namespace SpoofMessageService.Services;
+
+public interface IRuleParserService
+{
+    public long ParseRules(Rule[] rules);
+    public Rules Parse(Rule rule);
+}
