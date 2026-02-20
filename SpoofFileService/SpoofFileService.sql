894d9a08ce2ceebb83709162629c53de6a2a493c
Pychka <kronossluga@gmail.com>
2026-02-20 21:52:24 +0500

    script: add extension&category + change PK to fingerprint & add prefingerprint

--- SpoofFileService/SpoofFileService.sql
+++ SpoofFileService/SpoofFileService.sql
@@ -1,7 +1,28 @@
+create table "Extension"
+(
+	"Id" smallserial constraint "PK_Extension_Id" primary key,
+	"IsDeleted" boolean not null default false,
+	"Name" varchar(20)
+);
+
+create table "Category"
+(
+	"Id" smallserial constraint "PK_Category_Id" primary key,
+	"IsDeleted" boolean not null default false,
+	"Name" varchar(20)
+);
+
 create table "FileObject"
 (
-	"Id" uuid not null constraint "PK_FileObject_Id" primary key default uuidv7(),
+	"Id" bytea not null constraint "PK_FileObject_Id" primary key,
+	"PreFingerprint" bigint,
+	"CategoryId" smallint not null constraint "FK_FileObject_CategoryId" references "Category"("Id"),
+	"ExtensionId" smallint not null constraint "FK_FileObject_ExtensionId" references "Extension"("Id"),
 	"IsDeleted" boolean not null default false,
-	"FilePath" text not null,
-	"LastModified" timestamp not null default CURRENT_TIMESTAMP
+	"Path" text not null,
+	"Size" bigint not null,
+	"LastModified" timestamp not null default CURRENT_TIMESTAMP,
+	check(("PreFingerprint" is null and "Size" <= 50000000) or ("PreFingerprint" is not null and "Size" > 50000000)) --50mb
 );
+
+create index "IX_FileObject_Fast_Check" on "FileObject"("PreFingerprint", "Size");
