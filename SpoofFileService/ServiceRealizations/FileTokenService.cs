f343a3d2ed5e3da4c6488ddf84bbc5839341b107
Pychka <kronossluga@gmail.com>
2026-03-06 23:48:29 +0500

    feat: add FileTokenService for check token to access the file

---
+++ SpoofFileService/ServiceRealizations/FileTokenService.cs
@@ -0,0 +1,27 @@
+﻿using SecurityLibrary;
+using SpoofFileService.Services;
+
+namespace SpoofFileService.ServiceRealizations;
+
+public class FileTokenService : TokenService, IFileTokenService
+{
+    public bool IsValid(string token)
+    {
+        byte[]? tokenArray = Decrypt(token);
+        if(tokenArray == null)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public bool IsValid(ReadOnlySpan<byte> token)
+    {
+        byte[]? tokenArray = Decrypt(token);
+        if (tokenArray == null)
+        {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ SpoofFileService/Services/IFileTokenService.cs
@@ -0,0 +1,7 @@
+﻿namespace SpoofFileService.Services;
+
+public interface IFileTokenService
+{
+    public bool IsValid(string token);
+    public bool IsValid(ReadOnlySpan<byte> token);
+}
