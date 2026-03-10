20e90ff7fe961d320d03f1a2a8c97e110662c5a9
Pychka <kronossluga@gmail.com>
2026-03-10 22:29:07 +0500

    clean: remove unused scripts

--- SQLScripts/SpoofFileService.sql
+++
@@ -1,7 +0,0 @@
-create table "FileObject"
-(
-	"Id" uuid not null constraint "PK_FileObject_Id" primary key default uuidv7(),
-	"IsDeleted" boolean not null default false,
-	"FilePath" text not null,
-	"LastModified" timestamp not null default CURRENT_TIMESTAMP,
-);
